d25a3d28062bbe140585317776a8abbc5a0adb06
skrinskanastya <nastiolla@gmail.com>
2023-09-09 14:15:54 +0300

    image finder hw-4 fixed

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,9 +1,8 @@
 import { StyledItem } from './ImageGalleryItem.styled';
 export const ImageGalleryItem = ({ src, dataSrc, onImageClick }) => {
   return (
-    <StyledItem onClick={()=>(src)}>
+    <StyledItem onClick={()=>onImageClick(src)}>
       <img src={src} alt="" />
     </StyledItem>
   );
 };
-// data-src={dataSrc}
